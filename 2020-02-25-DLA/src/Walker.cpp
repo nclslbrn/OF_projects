bb110a1c3d73d043eac492b53b354dad3c67aeca
nclslbrn <nl@artemg.com>
2020-02-27 18:51:06 +0100

    fix no aggregation on tree

--- 2020-02-25-DLA/src/Walker.cpp
+++ 2020-02-25-DLA/src/Walker.cpp
@@ -1,56 +1,56 @@
 #include "Walker.h"
 
 Walker::Walker() {
-  pos.set(0, 0, 0);
-  size = 1;
-  isStuck = false;
+    pos.set(0, 0, 0);
+    size = 1;
+    isStuck = false;
 }
 //--------------------------------------------------------------
 
 Walker::Walker(ofVec3f _pos, float _size) {
-  pos = _pos;
-  size = _size;
-  isStuck = false;
+    pos = _pos;
+    size = _size;
+    isStuck = false;
 }
 //--------------------------------------------------------------
 
 void Walker::walk() {
-  int randomDirection = (int)ofRandom(1) % 6 + 1;
-  switch (randomDirection) {
-  case 0:
-    pos.x++;
-    break;
-  case 1:
-    pos.x--;
-    break;
-  case 2:
-    pos.y++;
-    break;
-  case 3:
-    pos.y--;
-    break;
-  case 4:
-    pos.z++;
-    break;
-  case 5:
-    pos.z--;
-    break;
-  }
+    int randomDirection = (int)ofRandom(1) % 6 + 1;
+    switch (randomDirection) {
+        case 0:
+            pos.x += size;
+            break;
+        case 1:
+            pos.x -= size;
+            break;
+        case 2:
+            pos.y += size;
+            break;
+        case 3:
+            pos.y -= size;
+            break;
+        case 4:
+            pos.z += size;
+            break;
+        case 5:
+            pos.z -= size;
+            break;
+    }
 }
 
 //--------------------------------------------------------------
-int Walker::isCloseTo(deque<Walker> walkers) {
-  for (int i = 0; i < walkers.size(); i++) {
-    float minDist = (size + walkers[i].size) / 2;
-    float dx = std::abs(pos.x - walkers[i].pos.x);
-    float dy = std::abs(pos.y - walkers[i].pos.y);
-    float dz = std::abs(pos.z - walkers[i].pos.z);
+int Walker::isCloseTo(deque<Walker> tree) {
+    for (int i = 0; i < tree.size(); i++) {
+        float minDist = (size + tree[i].size) / 2;
+        float dx = std::abs(tree[i].pos.x - pos.x);
+        float dy = std::abs(tree[i].pos.y - pos.y);
+        float dz = std::abs(tree[i].pos.z - pos.z);
 
-    if (dx <= minDist && dy <= minDist && dz <= minDist) {
-      isStuck = true;
-      return i;
-      break;
+        if (dx <= minDist && dy <= minDist && dz <= minDist) {
+            isStuck = true;
+            return i;
+            break;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
--- 2020-02-25-DLA/src/ofApp.cpp
+++ 2020-02-25-DLA/src/ofApp.cpp
@@ -1,69 +1,79 @@
 #include "ofApp.h"
 //--------------------------------------------------------------
 ofVec3f ofApp::randomPos() {
-  return ofVec3f(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()),
-                 ofRandom(ofGetHeight()));
+    return ofVec3f(ofRandom(ofGetWidth()), ofRandom(ofGetHeight()),
+                   ofRandom(ofGetHeight()));
 }
 //--------------------------------------------------------------
 ofVec3f ofApp::onRadius() {
-  ofVec3f center = ofVec3f(ofGetWidth()/2, ofGetHeight()/2, ofGetHeight()/2);
-  float theta = ofRandom(1) * glm::pi<float>() * 2;
-  float phi = glm::acos( 1 - ofRandom(1) * 20);
+    ofVec3f center = ofVec3f(ofGetWidth() / 2, ofGetHeight() / 2, ofGetHeight() / 2);
+    float theta = ofRandom(1) * glm::pi<float>() * 2;
+    float phi = glm::acos(1 - ofRandom(1) * 20);
 
-  center.x = glm::sin(phi) * glm::cos(theta) * radius;
-  center.y = glm::sin(phi) * glm::sin(theta) * radius;
-  center.z = glm::cos(phi) * radius;
+    center.x += glm::sin(phi) * glm::cos(theta) * radius;
+    center.y += glm::sin(phi) * glm::sin(theta) * radius;
+    center.z += glm::cos(phi) * radius;
 
-  return center;
+    return center;
 }
 //--------------------------------------------------------------
 void ofApp::setup() {
-  radius = ofGetHeight()/4;
-  steps = 12;
-  walkerNum = 15;
-  walkerSize = 5;
-  decrease = 0.995;
-
-  for (int i = 0; i < walkerNum; i++) {
-    ofVec3f newPos = onRadius();
-    Walker newWalker = Walker(newPos, walkerSize);
-    walkers.push_front(newWalker);
-  }
+    radius = min(ofGetWidth(), ofGetHeight()) / 3;
+    steps = 100;
+    walkerNum = 500;
+    walkerSize = 8;
+    decrease = 0.95;
+    ofVec3f center = ofVec3f(ofGetWidth() / 2, ofGetHeight() / 2, ofGetHeight() / 2);
+    Walker seed = Walker(center, walkerSize);
+    tree.push_front(seed);
+    for (int i = 0; i < walkerNum; i++) {
+        ofVec3f newPos = onRadius();
+        Walker newWalker = Walker(newPos, walkerSize);
+        walkers.push_front(newWalker);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-  for (int i = 0; i < walkerNum; i++) {
-    for (int t = 0; t <= steps; t++) {
-      walkers[i].walk();
-      if (walkers[i].isCloseTo(walkers) > 0) {
-        walkerSize *= decrease;
-        tree.push_front(walkers[i]);
+    for (int i = 0; i < walkerNum; i++) {
+        for (int t = 0; t <= steps; t++) {
+            walkers[i].walk();
+            if (walkers[i].isCloseTo(tree) != -1) {
+                tree.push_front(walkers[i]);
+                walkerSize *= decrease;
+                walkers[i].pos = onRadius();
+                walkers[i].size = walkerSize;
+            }
+        }
         walkers[i].pos = onRadius();
-        walkers[i].size = walkerSize;
-      }
     }
-  }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-  ofBackground(ofColor::black);
-  cam.begin();
-  ofPushMatrix();
-  ofTranslate(-ofGetWidth() / 2, -ofGetHeight() / 2, -ofGetHeight() / 1.5);
-  //--------------------------
-  ofSetColor(ofColor::white);
-  for (int i = 0; i < tree.size(); i++) {
-    ofDrawSphere(tree[i].pos.x, tree[i].pos.y, tree[i].pos.z, tree[i].size);
-  }
-  /*
-    ofSetColor(ofColor::tomato);
+    ofBackground(ofColor::black);
+    cam.begin();
+    ofPushMatrix();
+    ofTranslate(-ofGetWidth() / 2, -ofGetHeight() / 2, -ofGetHeight() / 2);
+    //--------------------------
+    ofSetColor(ofColor::white);
+    for (int i = 0; i < tree.size(); i++) {
+        ofDrawSphere(tree[i].pos.x, tree[i].pos.y, tree[i].pos.z, tree[i].size);
+        /* 
+        if (i + 1 < tree.size()) {
+            ofDrawLine(
+                tree[i].pos.x, tree[i].pos.y, tree[i].pos.z,
+                tree[i + 1].pos.x, tree[i + 1].pos.y, tree[i + 1].pos.z);
+        }
+         */
+    }
+
+    ofSetColor(ofColor::blue);
     for (int i = 0; i < walkers.size(); i++) {
-      ofDrawSphere(walkers[i].pos.x, walkers[i].pos.y, walkers[i].pos.z,
-                   walkers[i].size);
-    } */
-  //--------------------------
-  ofPopMatrix();
-  cam.end();
+        ofDrawSphere(walkers[i].pos.x, walkers[i].pos.y, walkers[i].pos.z,
+                     walkers[i].size);
+    }
+    //--------------------------
+    ofPopMatrix();
+    cam.end();
 }
