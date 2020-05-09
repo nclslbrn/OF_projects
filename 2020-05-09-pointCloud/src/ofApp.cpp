2af8b2c9cff552f3eb0ced7b02162d3785da58cd
nclslbrn <nl@artemg.com>
2020-05-09 17:00:21 +0200

    add shader

--- 2020-05-09-pointCloud/src/ofApp.cpp
+++ 2020-05-09-pointCloud/src/ofApp.cpp
@@ -13,6 +13,8 @@ void ofApp::setup() {
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_CULL_FACE);
 
+    cam.setAutoDistance(false);
+    cam.setDistance(12);
     streetModel.load("plys/GT_Madame1_2-from-meshlab.ply");
 
     point.setDiffuseColor(ofColor(255.0, 120.0, 120.0));
@@ -28,6 +30,7 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
     ofBackground(0, 0, 0);
+    ofEnableDepthTest();
     cam.begin();
     ofSetColor(200, 200, 200, 255);
     point.setPosition(0, 0, (float)ofGetHeight() * 0.75);
@@ -41,6 +44,18 @@ void ofApp::draw() {
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
+    // if user press c key
+    if (key == 99) {
+        ofVec3f camPos = cam.getPosition();
+        float rotation[4];
+        rotation[0] = cam.getOrientationQuat().x;
+        rotation[1] = cam.getOrientationQuat().y;
+        rotation[2] = cam.getOrientationQuat().z;
+        rotation[3] = cam.getOrientationQuat().w;
+
+        std::cout << "X:" << camPos.x << " Y:" << camPos.y << " Z:" << camPos.z << endl;
+        std::cout << "rX:" << rotation[0] << " rY:" << rotation[1] << " rZ:" << rotation[2] << " rW:" << rotation[3] << endl;
+    }
 }
 
 //--------------------------------------------------------------
