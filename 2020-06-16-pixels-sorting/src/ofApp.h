f0366933341b209b7973931a26752b362495e2d0
nclslbrn <nl@artem.com>
2020-06-27 23:36:00 +0200

    prepare settings to make mutiple HD output

--- 2020-06-16-pixels-sorting/src/ofApp.h
+++ 2020-06-16-pixels-sorting/src/ofApp.h
@@ -8,7 +8,12 @@ class ofApp : public ofBaseApp {
     void update();
     void draw();
     void keyPressed(int key);
-    void addLine(int x, int y, ofColor pixColor, int brightness, bool searchBrightPix);
+    void addLine(
+        int x,
+        int y,
+        ofColor pixColor,
+        int brightness,
+        bool searchBrightPix);
     void exit();
 
    private:
@@ -16,15 +21,15 @@ class ofApp : public ofBaseApp {
     int nextDarkY(int x, int y, int brightness);
     ofImage source;
     ofImage modified;
+    bool isSaved = false;
 
     int currY;
     int width, height;
-    int pixStep = 1;
-    int brightThreshold = 130;
-    int darkThreshold = 130;
-    int queryLigthThreshold = 150;
-    int brightnessVariation = 90;
-    bool isSaved = false;
-    string sourceName = "wolfgang-hasselmann-uDWerexH6RI-unsplash.jpg";
-    string sourceSize = "800x800";  // "1280x1280" "1080x1080" "800x800"  "400x400"
+    int pixStep = 2;
+    int brightThreshold = 180;
+    int darkThreshold = 70;
+    int queryLigthThreshold = 50;
+    int brightnessVariation = 100;
+    string sourceName = "3409485258_7b735fcedb_k.jpg";
+    string sourceSize = "800x800";  //  "1080x1080" "800x800"
 };
